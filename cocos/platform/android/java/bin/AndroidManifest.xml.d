82c0f95847b0897810c258f512e2dcb86821d119
Eugene Shcherbakov <crazyelk3D@gmail.com>
2016-02-10 14:26:47 +0300

    Файлы для сборки android, полученые при импорте chartboost

---
+++ cocos/platform/android/java/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/evgeniyshb/Developer/Projects/SlashingTrough/cocos2d/cocos/platform/android/java/bin/AndroidManifest.xml : \
+/Users/evgeniyshb/Developer/Projects/SlashingTrough/cocos2d/cocos/platform/android/java/AndroidManifest.xml \
---
+++ cocos/platform/android/java/bin/classes/com/enhance/gameservice/IGameTuningService.java.d
@@ -0,0 +1,3 @@
+/Users/evgeniyshb/Developer/Projects/SlashingTrough/cocos2d/cocos/platform/android/java/gen/com/enhance/gameservice/IGameTuningService.java: \
+  /Users/evgeniyshb/Developer/Projects/SlashingTrough/cocos2d/cocos/platform/android/java/src/com/enhance/gameservice/IGameTuningService.aidl 
+
